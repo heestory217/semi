d2e51a9b7445743c3b28c7fa61a542ffb3134bb1
heestory217 <heestory217@gmail.com>
2020-12-30 23:55:06 +0900

    포스트 dao, vo 수정

--- src/com/mytf/post/model/postDAO.java
+++ src/com/mytf/post/model/postDAO.java
@@ -14,56 +14,48 @@ import com.mytf.db.ConnectionPoolMgr2;
 
 public class postDAO {
 	private ConnectionPoolMgr2 pool;
-	
+
 	public postDAO() {
 		pool=new ConnectionPoolMgr2();
 	}
-	
+
 	public int insertPost(postVO vo) throws SQLException {
 		Connection con=null;
 		PreparedStatement ps=null;
 		try { 
 			//1,2. con
 			con=pool.getConnection();
-			
+
 			//3. ps
-		
+
 			/*
 			 * String
 			 * sql="insert into post(postNo, name, pwd, title, email, postContent )\r\n" +
 			 * "VALUES (postNo_seq.nextval, ?,?,?,?,?)";
 			 */
-			
-			  String
-			  sql="insert into post(postNo, projectNo, memberNo, title, postContent)"
-			  + " VALUES (postNo_seq.nextval, ?,?,?,?)";
-			  
-			  
-			  
-			 
-			  
+
+			String
+			sql="insert into post(postNo, projectNo, memberNo, title, postContent)"
+					+ " VALUES (postNo_seq.nextval, ?,?,?,?)";
+
 			ps=con.prepareStatement(sql);
-			
-				
+
 			ps.setString(1, vo.getProjectNo()); 
 			ps.setInt(2, vo.getMemberNo());	 
 			ps.setString(3, vo.getTitle());
 			ps.setString(4, vo.getPostContent());
-			
+
 			int cnt=ps.executeUpdate();
 			System.out.println("글쓰기 결과 cnt="+cnt+", 매개변수 vo="+vo);
-			
+
 			return cnt;
-			
-			
-			//4. exec
-			
+
 		}finally {
 			pool.dbClose(ps, con);
 		}
-		
+
 	}
-	
+
 	public List<postVO> selectAll(String projectNo) throws SQLException{
 		/*
 		 select * from board
@@ -72,76 +64,74 @@ public class postDAO {
 		 where title like '%안녕%'; 
 		 select * from board
 		 where content like '%내용%'; 
-		*/
-		
-		
+		 */
+
 		Connection con=null;
 		PreparedStatement ps=null;
 		ResultSet rs=null;
-		
+
 		List<postVO> list=new ArrayList<postVO>();
 		try {
 			//1,2 con
 			con=pool.getConnection();
-			
+
 			//3. ps
 			String sql="select * from post where projectNo = ?" +
 					" order by postNo desc";
 			ps=con.prepareStatement(sql);
-			
+
 			ps.setString(1, projectNo);
-		
+
 			//4. exec
 			rs=ps.executeQuery();
 			while(rs.next()){
-				
+
 				int postNo=rs.getInt("postNo");
 				projectNo=rs.getString("projectNo");
 				int memberNo=rs.getInt("memberNo");
 				String title=rs.getString("title");
 				String postContent=rs.getString("postContent");
 				Timestamp postDate=rs.getTimestamp("postDate");
-			
-				postVO vo = new postVO(postNo, projectNo, memberNo, title, postContent,postDate);
+
+				postVO vo = new postVO(postNo, projectNo, memberNo, title, postDate, postContent);
 				list.add(vo);
-			
+
 			}
 			System.out.println("글목록 결과 list.size=" + list.size()
-									+", 매개변수 projectNo="+projectNo);
+			+", 매개변수 projectNo="+projectNo);
 			return list;
 		}finally {
 			pool.dbClose(rs, ps, con);
-			
+
 		}
 	}
-	
+
 	public postVO selectByNo(String projectNo) throws SQLException {
 		Connection con=null;
 		PreparedStatement ps=null;
 		ResultSet rs=null;
-		
+
 		postVO vo = new postVO();
 		try {
 			//1,2
 			con=pool.getConnection();
-			
+
 			//3
 			String sql="select * from post where projectNo=?";
 			ps=con.prepareStatement(sql);
 			ps.setString(1, projectNo);
-			
+
 			//4
 			rs=ps.executeQuery();
 			if(rs.next()) {
-				
-				
+
 				vo.setPostNo(rs.getInt("postNo")); 
 				vo.setProjectNo(rs.getString("projectNo")); 
 				vo.setMemberNo(rs.getInt("memberNo")); 
 				vo.setTitle(rs.getString("title"));
 				vo.setPostContent(rs.getString("postContent"));
 				vo.setPostDate(rs.getTimestamp("postDate"));
-		
+
 			}
 			System.out.println("글 상세보기 결과 vo = "+vo + ",매개변수 projectNo=" + projectNo);
 			return vo;
@@ -153,19 +143,19 @@ public class postDAO {
 	public int updatePost(postVO vo) throws SQLException {
 		Connection con =null;
 		PreparedStatement ps=null;
-		
+
 		try {
 			//1,2
 			con=pool.getConnection();
-			
+
 			//3
 			String sql="update post" + 
-						" set title=?, postContent=?" +  //엔터값이 있을땐 맨앞에 한번 띄어쓰기 해주기
-						" where postNo=?";
-			
+					" set title=?, postContent=?" +  //엔터값이 있을땐 맨앞에 한번 띄어쓰기 해주기
+					" where postNo=?";
+
 			ps=con.prepareStatement(sql);
 
-			
+
 			ps.setString(1, vo.getTitle());
 			ps.setString(2, vo.getPostContent());
 			ps.setInt(3, vo.getPostNo());
@@ -173,107 +163,107 @@ public class postDAO {
 			//4
 			int cnt=ps.executeUpdate();
 			System.out.println("글수정 결과, cnt="+cnt+",매개변수 vo="+vo);
-			
+
 			return cnt;
-			
+
 		}finally {
 			pool.dbClose(ps, con);
-			
+
 		}
-		
+
 	}
-	
+
 	public int deletePost(int postNo, String pwd) throws SQLException {
 		Connection con=null;
 		PreparedStatement ps=null;
-		
-		
+
+
 		try {
 			//1,2
 			con=pool.getConnection();
-			
+
 			//3
 			String sql="delete from post where postNo=?";
 			ps=con.prepareStatement(sql);
 			ps.setInt(1, postNo);
-			
+
 			//4
 			int cnt=ps.executeUpdate();
 			System.out.println("글 삭제 결과, cnt="+cnt+", 매개변수 postNo=" + postNo);
-			
+
 			return cnt;
 		}finally {
 			pool.dbClose(ps, con);
 		}
 	}
-		
-	
-	
-		public List<postVO> selectMainNotice() throws SQLException{
-			Connection con=null;
-			PreparedStatement ps=null;
-			ResultSet rs=null;
-			
-			List<postVO> list=new ArrayList<postVO>();
-			try {
-				con=pool.getConnection();
-				
-				String sql="select * from (select postNo, title from post order by postNo desc) where rownum<=6";
-				ps=con.prepareStatement(sql);
-				
-				rs=ps.executeQuery();
-				while(rs.next()) {
-					int postNo=rs.getInt("postNo");
-					String title=rs.getString("title");
-					
-					postVO vo = new postVO();
-					vo.setPostNo(postNo);
-					vo.setTitle(title);
-					
-					list.add(vo);
-				}
-				System.out.println("메인 공지사항 조회 결과, list.size=" + list.size());
-				return list;
-			}finally {
-				pool.dbClose(rs, ps, con);
-				
+
+
+
+	public List<postVO> selectMainNotice() throws SQLException{
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+
+		List<postVO> list=new ArrayList<postVO>();
+		try {
+			con=pool.getConnection();
+
+			String sql="select * from (select postNo, title from post order by postNo desc) where rownum<=6";
+			ps=con.prepareStatement(sql);
+
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				int postNo=rs.getInt("postNo");
+				String title=rs.getString("title");
+
+				postVO vo = new postVO();
+				vo.setPostNo(postNo);
+				vo.setTitle(title);
+
+				list.add(vo);
 			}
+			System.out.println("메인 공지사항 조회 결과, list.size=" + list.size());
+			return list;
+		}finally {
+			pool.dbClose(rs, ps, con);
+
 		}
+	}
 
-		public postVO selectByPostNo(int postNo) throws SQLException {
-			Connection con=null;
-			PreparedStatement ps=null;
-			ResultSet rs=null;
-			
-			postVO vo = new postVO();
-			try {
-				//1,2
-				con=pool.getConnection();
-				
-				//3
-				String sql="select * from post where postNo=?";
-				ps=con.prepareStatement(sql);
-				ps.setInt(1, postNo);
-				
-				//4
-				rs=ps.executeQuery();
-				if(rs.next()) {
-					
-					
-					vo.setPostNo(rs.getInt("postNo")); 
-					vo.setProjectNo(rs.getString("projectNo")); 
-					vo.setMemberNo(rs.getInt("memberNo")); 
-					vo.setTitle(rs.getString("title"));
-					vo.setPostContent(rs.getString("postContent"));
-					vo.setPostDate(rs.getTimestamp("postDate"));
-			
-				}
-				System.out.println("글 상세보기 결과 vo = "+vo + ",매개변수 postNo=" + postNo);
-				return vo;
-			}finally {
-				pool.dbClose(rs, ps, con);
-			}
-			}
+	public postVO selectByPostNo(int postNo) throws SQLException {
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
 
-	
+		postVO vo = new postVO();
+		try {
+			//1,2
+			con=pool.getConnection();
+
+			//3
+			String sql="select * from post where postNo=?";
+			ps=con.prepareStatement(sql);
+			ps.setInt(1, postNo);
+
+			//4
+			rs=ps.executeQuery();
+			if(rs.next()) {
+
+
+				vo.setPostNo(rs.getInt("postNo")); 
+				vo.setProjectNo(rs.getString("projectNo")); 
+				vo.setMemberNo(rs.getInt("memberNo")); 
+				vo.setTitle(rs.getString("title"));
+				vo.setPostContent(rs.getString("postContent"));
+				vo.setPostDate(rs.getTimestamp("postDate"));
+
+			}
+			System.out.println("글 상세보기 결과 vo = "+vo + ",매개변수 postNo=" + postNo);
+			return vo;
+		}finally {
+			pool.dbClose(rs, ps, con);
+		}
+	}
+
+
 }
--- src/com/mytf/post/model/postVO.java
+++ src/com/mytf/post/model/postVO.java
@@ -6,76 +6,92 @@ public class postVO {
 	private int postNo;		     
 	private String projectNo;		     
 	private int memberNo;		     
-	private String name;		
-	private String pwd;			
 	private String title;			
-	private String email;		
 	private Timestamp postDate;	
 	private String postContent;
 	
 	
 	public postVO() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	public postVO(int postNo, String projectNo, int memberNo, String title,  String postContent,
-			Timestamp postDate) {
+	
+
+	public postVO(int postNo, String projectNo, int memberNo, String title, Timestamp postDate, String postContent) {
 		super();
 		this.postNo = postNo;
 		this.projectNo = projectNo;
 		this.memberNo = memberNo;
 		this.title = title;
-		this.postContent = postContent;
 		this.postDate = postDate;
+		this.postContent = postContent;
 	}
+
+
 	public int getPostNo() {
 		return postNo;
 	}
+
+
 	public void setPostNo(int postNo) {
 		this.postNo = postNo;
 	}
+
+
 	public String getProjectNo() {
 		return projectNo;
 	}
+
+
 	public void setProjectNo(String projectNo) {
 		this.projectNo = projectNo;
 	}
+
+
 	public int getMemberNo() {
 		return memberNo;
 	}
+
+
 	public void setMemberNo(int memberNo) {
 		this.memberNo = memberNo;
 	}
 
+
 	public String getTitle() {
 		return title;
 	}
+
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	public String getPostContent() {
-		return postContent;
-	}
-	public void setPostContent(String postContent) {
-		this.postContent = postContent;
-	}
+
+
 	public Timestamp getPostDate() {
 		return postDate;
 	}
+
+
 	public void setPostDate(Timestamp postDate) {
 		this.postDate = postDate;
 	}
+
+
+	public String getPostContent() {
+		return postContent;
+	}
+
+
+	public void setPostContent(String postContent) {
+		this.postContent = postContent;
+	}
+
+
 	@Override
 	public String toString() {
-		return "postVO [postNo=" + postNo + ", projectNo=" + projectNo + ", memberNo=" + memberNo + ", title=" + title +  ", postContent=" + postContent + ", postDate=" + postDate + "]";
+		return "postVO [postNo=" + postNo + ", projectNo=" + projectNo + ", memberNo=" + memberNo + ", title=" + title
+				+ ", postDate=" + postDate + ", postContent=" + postContent + "]";
 	}
-	
-
-	
-	
-	
-	
 	
 	
 }
